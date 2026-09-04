fce923ea050f79ca64dc09c4fbf126e8fb3ea2be
Grelani <greciaariaass@gmail.com>
2026-09-03 18:09:53 -0600

    Last modification

--- mintic/eda/__init__.py
+++ mintic/eda/__init__.py
@@ -1,4 +1,4 @@
-from .input_missing import impute_missing
+from .input_missing import input_missing
 from .detect_outliers import detect_outliers
 from .handle_outliers import handle_outliers
 from .plot_missing import plot_missing
